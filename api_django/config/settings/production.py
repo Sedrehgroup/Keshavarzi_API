f677aa0c7840f800564dbde6378dd23d805c9a06
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-15 10:52:35 +0330

    Very minor change for getting variable from env file.

--- api_django/config/settings/production.py
+++ api_django/config/settings/production.py
@@ -1,5 +1,5 @@
 from config.settings.base import *
 
 DEBUG = os.getenv("DEBUG", False)
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', " ").split()
 SECRET_KEY = os.environ.get("SECRET_KEY")
