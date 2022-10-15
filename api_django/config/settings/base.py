4fbfe56adff57a8cdad9b403b012727548c768d1
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-15 13:19:10 +0330

    Fixing AUTH_USER_MODEL(Invalid model reference)

--- api_django/config/settings/base.py
+++ api_django/config/settings/base.py
@@ -126,4 +126,4 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "users.model.User"
+AUTH_USER_MODEL = "users.User"
