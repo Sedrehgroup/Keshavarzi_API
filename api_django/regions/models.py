de4c5908c6596d8c71a8aceff5913ba76bfb455c
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-19 12:21:35 +0330

    Add missing file of "Start app 'regions'"

---
+++ api_django/regions/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.contrib.gis.db import models
+
+User = get_user_model()
+
+
+class Region(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    polygon = models.PolygonField()
+    name = models.CharField(max_length=20, help_text="Maximum length for this field is 20 character")
+    date_created = models.DateField()
+    is_active = models.BooleanField(default=True)  # ToDo: Is necessary or not?
+    images_dates = models.TextField()
