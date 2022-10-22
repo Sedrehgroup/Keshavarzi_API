84b40048d89d9f09e82d6f758e6d8a439030ea95
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-22 16:20:55 +0330

    Add __str__ method.

--- api_django/users/models.py
+++ api_django/users/models.py
@@ -7,8 +7,11 @@ class User(AbstractUser):
     username = None
     last_login = None
     email = models.EmailField(null=True, blank=True)
-    phone_number = models.CharField(max_length=13,default="+98", unique=True, help_text="Phone number should start with '+98'.\nThis field should contain 13 character.")
-    national_code = models.CharField(max_length=10,default="", help_text="National code should contain 10 character.")
+    phone_number = models.CharField(max_length=13, default="+98", unique=True, help_text="Phone number should start with '+98'.\nThis field should contain 13 character.")
+    national_code = models.CharField(max_length=10, default="", help_text="National code should contain 10 character.")
     is_expert = models.BooleanField(default=False)
 
     USERNAME_FIELD = "phone_number"
+
+    def __str__(self):
+        return self.get_full_name()
