04561c0ac87896ba8c97ca05a5644a3e58c5ba1e
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-22 11:49:50 +0330

    Customise User model.

--- api_django/users/models.py
+++ api_django/users/models.py
@@ -1,5 +1,14 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    is_staff = None
+    username = None
+    last_login = None
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=13,default="+98", unique=True, help_text="Phone number should start with '+98'.\nThis field should contain 13 character.")
+    national_code = models.CharField(max_length=10,default="", help_text="National code should contain 10 character.")
+    is_expert = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "phone_number"
