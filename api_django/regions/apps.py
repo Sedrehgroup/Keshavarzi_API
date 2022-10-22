8883830f2050a47612e82ee1bf4ad74585736038
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-22 16:20:35 +0330

    Write a signal for create a "Note" after connect expert to region.

--- api_django/regions/apps.py
+++ api_django/regions/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RegionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'regions'
+
+    def ready(self):
+        import regions.signals
---
+++ api_django/regions/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from regions.models import Region
+from users.models import User
+from notes.models import Note
+
+
+@receiver(post_save, sender=Region)
+def my_handler(sender, instance: Region, update_fields, **kwargs):
+    if update_fields is not None:
+        if ("expert_id" or "expert") in update_fields:  # Expert of region is updated
+            if instance.expert_id is not None:  # Expert is not deleted
+                note_text = f"کارشناس با شماره شناسایی {instance.expert_id} به ناحیه ای با شماره شناسایی {instance.id} متصل گردید."
+                Note.objects.create(region=instance, expert_id=instance.expert_id, text=note_text)
