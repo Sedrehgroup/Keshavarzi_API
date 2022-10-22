22c6ce179f961808a61985d9d273130e046c75bf
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-22 16:57:12 +0330

    Write new permission to authorize the expert user.

--- api_django/users/permissions.py
+++ api_django/users/permissions.py
@@ -8,3 +8,12 @@ class IsSuperUser(BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_superuser)
+
+
+class IsExpertUser(BasePermission):
+    """
+    Allows access only to expert users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_expert)
