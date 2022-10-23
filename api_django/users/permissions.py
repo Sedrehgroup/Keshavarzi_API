14aa41c426fbe82d0c1147d6af2b60be36ae50c0
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-23 13:50:32 +0330

    Create new permission to check that user is a regular user.

--- api_django/users/permissions.py
+++ api_django/users/permissions.py
@@ -17,3 +17,17 @@ class IsExpertUser(BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_expert)
+
+
+class IsRegularUser(BasePermission):
+    """
+    Allows access only to regular users.
+
+    ** tip **
+        Regular user => Is not super user and is not expert user
+    """
+
+    def has_permission(self, request, view):
+        if not request.user:
+            return False
+        return not (request.user.is_superuser or request.user.is_expert)
