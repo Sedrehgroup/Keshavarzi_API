daa9d2a9889982c5a32cb4d7a060c182f9d623fe
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-23 13:52:41 +0330

    Delete '/' from url of CreateUser.

--- api_django/users/api/urls.py
+++ api_django/users/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.api.views import CustomTokenObtainPairView,CreateUser
+from users.api.views import CustomTokenObtainPairView, CreateUser
 
 app_name = "users"
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('create/', CreateUser.as_view(), name='create'),
+    path('', CreateUser.as_view(), name='create'),
 ]
