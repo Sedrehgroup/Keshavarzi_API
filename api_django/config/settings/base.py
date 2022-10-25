ee6816db8b490621ec366e2e1049536e038bf505
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-25 17:38:12 +0330

    Convert single quote to double quote.

--- api_django/config/settings/base.py
+++ api_django/config/settings/base.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASS"),
