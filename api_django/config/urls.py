fd2d4dd6efd5d4cfabe6611a98393dc0348ed52e
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-31 11:26:15 +0330

    Add urls of drf_yasg

--- api_django/config/urls.py
+++ api_django/config/urls.py
@@ -17,8 +17,28 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Jaseci API",
+        default_version='v1',
+        description="Welcome to the world of Jaseci",
+        terms_of_service="https://www.jaseci.org",
+        contact=openapi.Contact(email="jason@jaseci.org"),
+        license=openapi.License(name="Awesome IP"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),  # <-- Here
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # <-- Here
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # <-- Here
+
     path('admin/', admin.site.urls),
     path("users/", include("users.api.urls", namespace="users")),
     path("regions/", include("regions.api.urls", namespace="regions")),
