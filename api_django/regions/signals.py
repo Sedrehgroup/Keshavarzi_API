f78b1b2b4a86b96b7814b313ee0b777343686d9f
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-31 11:20:28 +0330

    Write new signal. (download_images_after_region)[Not working as expected]

--- api_django/regions/signals.py
+++ api_django/regions/signals.py
@@ -1,8 +1,10 @@
+import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from regions.models import Region
 from notes.models import Note
+from regions.utils import get_geojson_by_polygon
 
 
 @receiver(post_save, sender=Region)
@@ -13,3 +15,12 @@ def create_note_after_attach_expert(sender, instance: Region, update_fields, **k
                 note_text = f"کارشناس با شماره شناسایی {instance.expert_id} به ناحیه ای با شماره شناسایی {instance.id} متصل گردید."
                 Note.objects.create(region=instance, user_id=instance.expert_id,
                                     text=note_text, user_role="E")
+
+
+@receiver(post_save, sender=Region)
+def download_images_after_region(sender, instance: Region, **kwargs):
+    end = datetime.datetime.now()
+    start = end - datetime.timedelta(days=30)
+    geom = get_geojson_by_polygon(instance.polygon)
+    # download_images.delay(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'),
+    #                       geom, instance.user_id, instance.id)
