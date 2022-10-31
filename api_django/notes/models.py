7bdd49e2e9927b597a7972253d61eb32856d6888
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-31 11:13:46 +0330

    Convert "expert" field to "user"

--- api_django/notes/models.py
+++ api_django/notes/models.py
@@ -8,7 +8,7 @@ from users.models import User
 
 class Note(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
-    expert = models.ForeignKey(User, limit_choices_to={'is_expert': True}, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField(default=now)
     updated_date = models.DateTimeField(null=True, blank=True)
