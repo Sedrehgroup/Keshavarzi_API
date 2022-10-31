fb369bd05a42f22f3fd771d66c3813c1ec2ae54b
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-31 11:19:38 +0330

    Based on change "expert" to "user" in Note, I changed some fields in create_note_after_attach_expert signal.

--- api_django/regions/signals.py
+++ api_django/regions/signals.py
@@ -2,7 +2,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from regions.models import Region
-from users.models import User
 from notes.models import Note
 
 
@@ -12,4 +11,5 @@ def create_note_after_attach_expert(sender, instance: Region, update_fields, **k
         if ("expert_id" or "expert") in update_fields:  # Expert of region is updated
             if instance.expert_id is not None:  # Expert is attached
                 note_text = f"کارشناس با شماره شناسایی {instance.expert_id} به ناحیه ای با شماره شناسایی {instance.id} متصل گردید."
-                Note.objects.create(region=instance, expert_id=instance.expert_id, text=note_text)
+                Note.objects.create(region=instance, user_id=instance.expert_id,
+                                    text=note_text, user_role="E")
