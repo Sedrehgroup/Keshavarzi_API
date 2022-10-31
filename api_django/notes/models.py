16d2f1e7a9acda1c6d7687ed0d323ddd821d89f8
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-31 11:14:14 +0330

    Add user_role field to the Note model.

--- api_django/notes/models.py
+++ api_django/notes/models.py
@@ -5,10 +5,17 @@ from regions.models import Region
 
 from users.models import User
 
+USER_ROLE_CHOICES = (
+    ("U", "User"),
+    ("E", "Expert"),
+    ("A", "Admin")
+)
+
 
 class Note(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_role = models.CharField(max_length=1, choices=USER_ROLE_CHOICES)
     text = models.TextField()
     created_date = models.DateTimeField(default=now)
     updated_date = models.DateTimeField(null=True, blank=True)
