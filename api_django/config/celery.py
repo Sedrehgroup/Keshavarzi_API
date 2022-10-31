1283c9826375065a0c942b082d365e29e286966b
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-10-31 11:59:29 +0330

    Celery configuration.

--- api_django/config/celery.py
+++ api_django/config/celery.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from celery import Celery
 
@@ -11,6 +12,17 @@ app = Celery('FirstPrj')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(['regions.tasks'])
+
+app.conf.broker_url = 'amqp://rabbitmq'
+app.conf.result_backend = 'rpc://'
+app.conf.task_serializer = 'json'
+app.conf.result_serializer = 'json'
+app.conf.accept_content = ['json']
+app.conf.result_expires = timedelta(days=1)
+app.conf.task_always_eager = False
+app.conf.worker_prefetch_multiplier = 1
+app.conf.CELERY_BEAT_SCHEDULER = 'django-celery-beat.schedulers.DatabaseScheduler'
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
