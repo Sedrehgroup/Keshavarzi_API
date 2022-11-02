5fbc80c7c5265ddcdddf67de1cc68d0cf1089550
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-02 17:45:27 +0330

    Minor change in update dates of region.

--- api_django/regions/tasks.py
+++ api_django/regions/tasks.py
@@ -54,4 +54,7 @@ def download_images(start, end, geom, user_id, region_id, dates):
                 raster_file.write(response.content)
 
         cat.create_coveragestore(name=f"user_{user_id}--region_{region_id}--{img_date}", data=file_path)
-        Region.objects.filter(id=region_id).update(dates=dates)
+        # Region.objects.filter(id=region_id).update(dates=dates) ToDo
+        region = Region.objects.get(id=region_id)
+        region.dates = dates
+        region.save(update_fields=["dates"])
