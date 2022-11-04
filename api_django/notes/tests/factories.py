fb58bc2b7685a6af06d3e960db8ebcc4fab47cb3
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-04 20:52:02 +0330

    Create note without random user(expert or normal user)

--- api_django/notes/tests/factories.py
+++ api_django/notes/tests/factories.py
@@ -2,13 +2,19 @@ import factory
 
 from notes.models import Note
 from regions.tests.factories import RegionFactory
-from users.tests.factories import UserFactory
 
 
 class NoteFactory(factory.django.DjangoModelFactory):
     region = factory.SubFactory(RegionFactory)
-    user = factory.SubFactory(UserFactory)
     text = factory.Faker("text", max_nb_chars=200)
+
+    @classmethod
+    def create(cls, regions_expert_as_user=False, **kwargs):
+        if regions_expert_as_user:
+            kwargs['user_id'] = kwargs["region"].expert_id
+        else:
+            kwargs['user_id'] = kwargs["region"].user_id
+        return super(NoteFactory, cls).create(**kwargs)
 
     class Meta:
         model = Note
