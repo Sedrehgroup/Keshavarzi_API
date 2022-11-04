65d3003032c4a16495b5e3be6087cf0baf456b86
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-04 19:47:33 +0330

    ✔✔ Test that key_data is reading correctly

--- api_django/utils/gee/utils.py
+++ api_django/utils/gee/utils.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 private_key_path = os.path.join(settings.BASE_DIR, "utils", "gee", "geotest-privkey.json")
 if not os.path.isfile(private_key_path):
     raise FileNotFoundError("geotest-privkey.json not found. This file should exists in utils.gee package.")
-#Test
+
 service_account = 'geo-test@geotest-317218.iam.gserviceaccount.com'
 with open(private_key_path, 'r') as pk:
     credentials = ee.ServiceAccountCredentials(service_account, key_data=json.dumps(json.load(pk)))
