e840ae526222ab7c22098de0ec354a5786fb3359
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-04 20:03:41 +0330

    Missing line from commit

--- api_django/notes/api/views.py
+++ api_django/notes/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from notes.api.serializers import CreateNoteSerializer, UpdateNoteSerializer
