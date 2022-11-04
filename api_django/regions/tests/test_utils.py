b6521ea60b601b0baa47e29b7bbe22331229ebdd
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-04 14:03:56 +0330

    Remove unused comments.

--- api_django/regions/tests/test_utils.py
+++ api_django/regions/tests/test_utils.py
@@ -12,48 +12,6 @@ class RegionUtilsTestCase(APITestCase):
         self.gee_polygon = ee.Geometry.Polygon(coords=fake_polygon_geojson['features'][0]['geometry']['coordinates'])
         self.start, self.end = ("2022-02-01", "2022-03-01")
 
-    # def test_get_regions_id_date(self):
-    #     date_range = ("2021-01-01", "2021-02-01")
-    #     geom = {
-    #         "type": "FeatureCollection",
-    #         "features": [
-    #             {
-    #                 "type": "Feature",
-    #                 "properties": {},
-    #                 "geometry": {
-    #                     "coordinates": [
-    #                         [
-    #                             [
-    #                                 51.38587730337923,
-    #                                 35.705011566456065
-    #                             ],
-    #                             [
-    #                                 51.38587730337923,
-    #                                 35.68850620569404
-    #                             ],
-    #                             [
-    #                                 51.410752479782275,
-    #                                 35.68850620569404
-    #                             ],
-    #                             [
-    #                                 51.410752479782275,
-    #                                 35.705011566456065
-    #                             ],
-    #                             [
-    #                                 51.38587730337923,
-    #                                 35.705011566456065
-    #                             ]
-    #                         ]
-    #                     ],
-    #                     "type": "Polygon"
-    #                 }
-    #             }
-    #         ]
-    #     }
-    #
-    #     id_date_list = utils.get_regions_id_date(geom, date_range)
-    #     self.assertEqual(len(id_date_list), 6)
-
     def test_geom_validation(self):
         """ Test that get_and_validate_polygon_by_geom is working as expected """
         without_features = {"type": "FeatureCollection"}
