e6b9020bb29fe164dfe894f8758c8927e912098a
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-04 19:34:14 +0330

    -- Test that key_data is reading correctly

--- api_django/utils/gee/utils.py
+++ api_django/utils/gee/utils.py
@@ -15,7 +15,7 @@ if not os.path.isfile(private_key_path):
 #Test
 service_account = 'geo-test@geotest-317218.iam.gserviceaccount.com'
 with open(private_key_path, 'r') as pk:
-    credentials = ee.ServiceAccountCredentials(service_account, key_data=json.loads(json.dumps(pk)))
+    credentials = ee.ServiceAccountCredentials(service_account, key_data=json.load(pk))
 ee.Initialize(credentials)
 
 
