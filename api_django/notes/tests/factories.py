797228fa85043f557b4ec608262f9730647869c7
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-05 16:16:36 +0330

    Delete create method from NoteFactory.

--- api_django/notes/tests/factories.py
+++ api_django/notes/tests/factories.py
@@ -8,13 +8,5 @@ class NoteFactory(factory.django.DjangoModelFactory):
     region = factory.SubFactory(RegionFactory)
     text = factory.Faker("text", max_nb_chars=200)
 
-    @classmethod
-    def create(cls, regions_expert_as_user=False, **kwargs):
-        if regions_expert_as_user:
-            kwargs['user_id'] = kwargs["region"].expert_id
-        else:
-            kwargs['user_id'] = kwargs["region"].user_id
-        return super(NoteFactory, cls).create(**kwargs)
-
     class Meta:
         model = Note
