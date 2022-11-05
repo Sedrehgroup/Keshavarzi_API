3ac0e40f766d20d21e83b12d2a4f57884037b598
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-05 16:25:40 +0330

    Add user to NoteFactory.

--- api_django/notes/tests/factories.py
+++ api_django/notes/tests/factories.py
@@ -2,10 +2,12 @@ import factory
 
 from notes.models import Note
 from regions.tests.factories import RegionFactory
+from users.tests.factories import UserFactory
 
 
 class NoteFactory(factory.django.DjangoModelFactory):
     region = factory.SubFactory(RegionFactory)
+    user = factory.SubFactory(UserFactory)
     text = factory.Faker("text", max_nb_chars=200)
 
     class Meta:
