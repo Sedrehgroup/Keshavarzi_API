360c4fc41dfe10a63070c6d40c37da71b70bcadb
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-05 16:02:20 +0330

    Missing import from commit.

--- api_django/notes/api/views.py
+++ api_django/notes/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from notes.api.serializers import CreateNoteSerializer, UpdateNoteSerializer
