a77fb53c11fe8850d2c33552141e05206120311e
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-06 19:09:51 +0330

    Add has_permission to custom permissions

--- api_django/regions/permissions.py
+++ api_django/regions/permissions.py
@@ -5,7 +5,15 @@ class IsRegionUser(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user_id == request.user.id
 
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(user and user.is_authenticated and not (user.is_expert or user.is_admin))
+
 
 class IsRegionExpert(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.expert_id == request.user.id
+
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(user and user.is_authenticated and user.is_expert)
