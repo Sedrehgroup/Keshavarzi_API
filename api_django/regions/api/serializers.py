8e1cc3bcdc9ff7dc12125258eac702194c48c20c
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-06 17:14:56 +0330

    Minor code cleaning.

--- api_django/regions/api/serializers.py
+++ api_django/regions/api/serializers.py
@@ -1,9 +1,7 @@
-from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from regions.models import Region
-from regions.tests.factories import fake_polygon_geojson
 from regions.utils import get_geojson_by_polygon, get_polygon_by_geojson
 from users.api.serializers import UserSerializer
 from users.models import User
