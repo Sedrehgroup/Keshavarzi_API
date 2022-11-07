3baf650da633c2f705e0e8b5b14a8e2d0ece0fa2
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-07 11:34:03 +0330

    Set message for permissions

--- api_django/regions/permissions.py
+++ api_django/regions/permissions.py
@@ -2,10 +2,14 @@ from rest_framework.permissions import BasePermission
 
 
 class IsRegionUser(BasePermission):
+    message = "This user is not related to region"
+
     def has_object_permission(self, request, view, obj):
         return obj.user_id == request.user.id
 
 
 class IsRegionExpert(BasePermission):
+    message = "This expert user is not related to region"
+
     def has_object_permission(self, request, view, obj):
         return obj.expert_id == request.user.id
