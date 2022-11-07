7208e9ce46bb8bb2e2b3c4cdf1423bdc41f08bbc
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-07 12:55:33 +0330

    -- Update schema.

--- api_django/notes/api/views.py
+++ api_django/notes/api/views.py
@@ -42,13 +42,10 @@ class ListCreateNote(ListCreateAPIView):
 
 @extend_schema_view(get=extend_schema(summary="List notes by region ID",
                                       description="List created notes that are related to a specific region.",
-                                      examples=[OpenApiExample(name="Region", summary="Region not found", response_only=True,
-                                                               value=[{"Region": "Region with given ID is not exists."}]),
-                                                OpenApiExample(name="Note", summary="Note not found", response_only=True,
-                                                               value=[{"Notes": "We didn't find any matching note."}])],
                                       responses={200: OpenApiResponse(ListNotesByRegionSerializer),
                                                  403: OpenApiResponse(description="User is not authenticated.\nUser doesn't have permission to access this region."),
-                                                 404: OpenApiResponse(ListNotesByRegionSerializer,description="Region or any note, not found",examples=[OpenApiExample("Example1", value={"field": 1})])
+                                                 404: OpenApiResponse(description="{'Region': 'Region with given ID is not exists.'}"
+                                                                                  "{'Notes': 'We didn't find any matching note.'}")
                                                  }))
 class ListNotesByRegion(ListAPIView):
     permission_classes = [IsAuthenticated, IsRegionUser | IsRegionExpert | IsAdmin]
