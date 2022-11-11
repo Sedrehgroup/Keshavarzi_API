e5061f5c63eb52b051e89e2330656a1a3a95ee3b
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-11 18:04:42 +0330

    ❌❌ Add doc endpoint

--- api_django/config/urls.py
+++ api_django/config/urls.py
@@ -18,24 +18,9 @@ from django.urls import include, path
 from django.conf import settings
 
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView, SpectacularYAMLAPIView
 
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Keshavarzi",
-#         default_version='1.0.0',
-#         description="Created by Sedreh group",
-#         contact=openapi.Contact(email="sedreh@gmail.com"),  # ToDo
-#     ),
-#     public=True,
-#     permission_classes=[permissions.AllowAny],
-# )
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
-    path('doc/', SpectacularYAMLAPIView.as_view(), name='document'),
 
     path('admin/', admin.site.urls),
     path("users/", include("users.api.urls", namespace="users")),
