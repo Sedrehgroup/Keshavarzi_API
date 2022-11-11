5173dcc7794538bca9f974578fd83e87707ee6b4
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-11 15:50:42 +0330

    Add new endpoint to verify the access token.

--- api_django/users/api/urls.py
+++ api_django/users/api/urls.py
@@ -8,6 +8,7 @@ app_name = "users"
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', CreateUser.as_view(), name='create'),
     path('<int:pk>/', RetrieveUser.as_view(), name='retrieve'),
 ]
