86f0aeed4f18ed9b16705c4cad256b261f08caeb
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-12 17:04:53 +0330

    -- Test download time

--- api_django/regions/task_test.py
+++ api_django/regions/task_test.py
@@ -8,6 +8,15 @@ import os
 from django.conf import settings
 from django.utils.timezone import now
 
+private_key_path = os.path.join(settings.BASE_DIR, "utils", "gee", "geotest-privkey.json")
+if not os.path.isfile(private_key_path):
+    raise FileNotFoundError("geotest-privkey.json not found. This file should exists in utils.gee package.")
+
+service_account = 'geo-test@geotest-317218.iam.gserviceaccount.com'
+with open(private_key_path, 'r') as pk:
+    credentials = ee.ServiceAccountCredentials(service_account, key_data=json.dumps(json.load(pk)))
+ee.Initialize(credentials)
+
 geojson = {
   "type": "FeatureCollection",
   "features": [
