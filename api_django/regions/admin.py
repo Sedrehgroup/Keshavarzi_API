367fadc558cef6472234010de13f3431599a7819
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-12 19:53:37 +0330

    Some customization in admin panel.

--- api_django/regions/admin.py
+++ api_django/regions/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
 
 from regions.models import Region
 
 
 @admin.register(Region)
-class RegionAdmin(admin.ModelAdmin):
-    pass
+class RegionAdmin(GISModelAdmin):
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_zoom': 17,
+            'default_lon': 51.395509,
+            'default_lat': 35.707619,
+        },
+    }
