995dc822936bd2fa815cad30cdf638f7014d5764
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-12 20:28:46 +0330

    set handler.console.level to DEBUG

--- api_django/config/settings/base.py
+++ api_django/config/settings/base.py
@@ -177,7 +177,7 @@ LOGGING = {
     },
     'handlers': {
         'console': {
-            'level': 'INFO',
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
         },
         'mail_admins': {
