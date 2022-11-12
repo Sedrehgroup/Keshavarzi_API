4d1b7415b4e62f4c8a897c6ca1a5faa3770e4a36
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-12 19:30:37 +0330

    Add gis to INSTALLED_APPS

--- api_django/config/settings/base.py
+++ api_django/config/settings/base.py
@@ -38,6 +38,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis'
 ]
 LOCAL_APPS = [
     "users.apps.UsersConfig",
