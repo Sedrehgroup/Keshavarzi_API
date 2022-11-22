9ed68a8d93fd3fe3c677c0f7adb5d109df3b73be
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-22 17:01:01 +0330

    Write some config for celery

--- operator/celery/tasks.py
+++ operator/celery/tasks.py
@@ -4,3 +4,13 @@ from datetime import timedelta
 from celery import Celery
 
 celery_app = Celery('config')
+
+celery_app.conf.broker_url = 'amqp://rabbitmq'
+celery_app.conf.result_backend = 'rpc://'
+celery_app.conf.task_serializer = 'json'
+celery_app.conf.result_serializer = 'json'
+celery_app.conf.accept_content = ['json']
+celery_app.conf.result_expires = timedelta(days=1)
+celery_app.conf.task_always_eager = False
+celery_app.conf.worker_prefetch_multiplier = 1
+
