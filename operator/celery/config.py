de2d61a33490a4cb05cf1f1d96e1920a13ced528
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-11-24 15:04:34 +0330

    Get configuration data of rabbitmq

--- operator/celery/config.py
+++ operator/celery/config.py
@@ -5,7 +5,7 @@ from celery import Celery
 
 celery_app = Celery('config')
 
-celery_app.conf.broker_url = 'amqp://rabbitmq'
+celery_app.conf.broker_url = f"amqp://{os.environ['RABBITMQ_DEFAULT_USER']}:{os.environ['RABBITMQ_DEFAULT_PASS']}@{os.environ['SERVER_IP']}"
 celery_app.conf.result_backend = 'rpc://'
 celery_app.conf.task_serializer = 'json'
 celery_app.conf.result_serializer = 'json'
@@ -13,4 +13,3 @@ celery_app.conf.accept_content = ['json']
 celery_app.conf.result_expires = timedelta(days=1)
 celery_app.conf.task_always_eager = False
 celery_app.conf.worker_prefetch_multiplier = 1
-
