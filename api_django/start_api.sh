0ba094d7a15e801d00734953af30e24439d3a652
shayestehhs <shayesthhs1@gmail.com>
2022-12-05 15:53:32 +0330

    Create startup command for api_django

---
+++ api_django/start_api.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+python manage.py makemigrations --noinput
+python manage.py migrate
+python manage.py collectstatic --no-input
+gunicorn --reload config.wsgi --bind 0.0.0.0:8000
