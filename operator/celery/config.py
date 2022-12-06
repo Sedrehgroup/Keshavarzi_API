8bf44ed53b3ee1f58a6d00cd87967dbb0373f06f
shayestehhs <shayesthhs1@gmail.com>
2022-12-06 16:02:12 +0330

    Write some new logs for download_images task

--- operator/celery/config.py
+++ operator/celery/config.py
@@ -50,15 +50,19 @@ def download_images(start, end, polygon_geojson, user_id, region_id, dates):
     dates = dates if dates else ""
     logger.debug("Start downloading images")
     for img_id, img_date in id_date_list:
+        logger.debug(f"Start image with {img_date} date")
         url = ee.Image(img_id).getDownloadURL(params={
             'scale': 10, 'region': polygon,
             "bands": ['TCI_R', 'TCI_G', 'TCI_B', 'B4', 'B8'],
             'crs': 'EPSG:4326', 'filePerBand': False, 'format': 'GEO_TIFF'})
 
-        ndvi_file_path = region_tools.get_ndvi_path(img_date)  # Example: /media/images/user-1/region-1/ndvi/2022-01-02.tif
+        ndvi_file_path = region_tools.get_ndvi_path(
+            img_date)  # Example: /media/images/user-1/region-1/ndvi/2022-01-02.tif
         rgb_file_path = region_tools.get_rgb_path(img_date)  # Example: /media/images/user-1/region-1/rgb/2022-01-02.tif
         dates += f"{img_date}\n"
+        logger.debug("Start downloading")
         with requests.get(url) as response:
+            logger.debug("End downloading")
             with BytesIO(response.content) as content:
                 with rasterio.open(content) as raster_file:
                     b4, b8 = raster_file.read(4), raster_file.read(5)  # ['TCI_R', 'TCI_G', 'TCI_B', 'B4', 'B8']
@@ -67,15 +71,20 @@ def download_images(start, end, polygon_geojson, user_id, region_id, dates):
 
                     meta = raster_file.meta
                     meta.update({"width": raster_file.width, "height": raster_file.height,
-                                 "driver": 'GTiff', "dtype": rasterio.float32})
-
+                                 "driver": 'GTiff', "dtype": rasterio.uint8})
+                    logger.debug(meta)  # ToDo: delete this line
         with rasterio.open(ndvi_file_path, "w", **meta) as ndvi_file:
+            logger.debug("Write NDVI file")
             ndvi_file.write(ndvi_result, 1)
         with rasterio.open(rgb_file_path, "w", **meta) as rgb_file:
+            logger.debug("Write RGB  file")
             rgb_file.write(rgb_result, (1, 2, 3))
 
         try:
+            logger.debug("Send NDVI file to geoserver")
             cat.create_coveragestore(name=f"user_{user_id}--region_{region_id}--NDVI--{img_date}", data=ndvi_file_path)
+            logger.debug("Send RGB file to geoserver")
             cat.create_coveragestore(name=f"user_{user_id}--region_{region_id}--RGB--{img_date}", data=rgb_file_path)
         except ConflictingDataError as e:
             logger.error(e)
+        logger.debug("END sending files")
