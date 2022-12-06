35a97b9c5d8750745ba614ea16d7590fd1ef649f
shayestehhs <shayesthhs1@gmail.com>
2022-12-06 17:56:11 +0330

    Fix

--- operator/celery/config.py
+++ operator/celery/config.py
@@ -56,8 +56,7 @@ def download_images(start, end, polygon_geojson, user_id, region_id, dates):
             "bands": ['TCI_R', 'TCI_G', 'TCI_B', 'B4', 'B8'],
             'crs': 'EPSG:4326', 'filePerBand': False, 'format': 'GEO_TIFF'})
 
-        ndvi_file_path = region_tools.get_ndvi_path(
-            img_date)  # Example: /media/images/user-1/region-1/ndvi/2022-01-02.tif
+        ndvi_file_path = region_tools.get_ndvi_path(img_date)  # Example: /media/images/user-1/region-1/ndvi/2022-01-02.tif
         rgb_file_path = region_tools.get_rgb_path(img_date)  # Example: /media/images/user-1/region-1/rgb/2022-01-02.tif
         dates += f"{img_date}\n"
         logger.debug("Start downloading")
@@ -66,17 +65,19 @@ def download_images(start, end, polygon_geojson, user_id, region_id, dates):
             with BytesIO(response.content) as content:
                 with rasterio.open(content) as raster_file:
                     b4, b8 = raster_file.read(4), raster_file.read(5)  # ['TCI_R', 'TCI_G', 'TCI_B', 'B4', 'B8']
+                    b4 = b4.astype('float16')  # ToDo: Delete this line and change format of gee image
+                    b8 = b8.astype('float16')  # ToDo: Delete this line and change format of gee image
                     ndvi_result = (b8 - b4) / (b8 + b4)
                     rgb_result = raster_file.read((1, 2, 3))
 
                     meta = raster_file.meta
+                    meta.pop("count")
                     meta.update({"width": raster_file.width, "height": raster_file.height,
                                  "driver": 'GTiff', "dtype": rasterio.uint8})
-                    logger.debug(meta)  # ToDo: delete this line
-        with rasterio.open(ndvi_file_path, "w", **meta) as ndvi_file:
+        with rasterio.open(ndvi_file_path, "w", **meta, count=1) as ndvi_file:
             logger.debug("Write NDVI file")
             ndvi_file.write(ndvi_result, 1)
-        with rasterio.open(rgb_file_path, "w", **meta) as rgb_file:
+        with rasterio.open(rgb_file_path, "w", **meta, count=3) as rgb_file:
             logger.debug("Write RGB  file")
             rgb_file.write(rgb_result, (1, 2, 3))
 
