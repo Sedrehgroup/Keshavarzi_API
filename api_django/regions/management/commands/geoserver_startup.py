273855231eea626185a09fad6bc9cbfc010e7e0a
shayestehhs <shayesthhs1@gmail.com>
2022-12-06 12:21:49 +0330

    Delete raw script for geoserver_startup.py

--- api_django/regions/management/commands/geoserver_startup.py
+++ api_django/regions/management/commands/geoserver_startup.py
@@ -1,12 +1,16 @@
+import logging
+
 from django.core.management.base import BaseCommand, CommandError
 from os import environ
 from geoserver.catalog import Catalog
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Run startup commands for Geoserver'
 
     def handle(self, *args, **options):
+        logger.info("Start: Geoserver startup")
         cat = Catalog(f'http://{environ["GEOSERVER_HOST"]}:{environ["GEOSERVER_PORT"]}/geoserver/rest/',
                       username=environ["GEOSERVER_USER"],
                       password=environ["GEOSERVER_PASS"])
@@ -15,3 +19,4 @@ class Command(BaseCommand):
         if not cat.get_workspace(default_workspace_name):
             cat.create_workspace(default_workspace_name)
         cat.set_default_workspace(default_workspace_name)
+        logger.info("End: Geoserver startup")
