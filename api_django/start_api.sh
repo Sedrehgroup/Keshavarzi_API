77fdbb817a72e002a25e8ad529d2791e31523ec3
shayestehhs <shayesthhs1@gmail.com>
2022-12-06 12:57:09 +0330

    Change order of commands

--- api_django/start_api.sh
+++ api_django/start_api.sh
@@ -7,6 +7,7 @@ python manage.py wait_for_db
 python manage.py makemigrations --noinput
 python manage.py migrate
 python manage.py collectstatic --no-input
-gunicorn --reload config.wsgi --bind 0.0.0.0:8000
 
 python manage.py geoserver_startup
+
+gunicorn --reload config.wsgi --bind 0.0.0.0:8000
