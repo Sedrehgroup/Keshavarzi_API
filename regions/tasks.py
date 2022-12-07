f312db2cee8fd67d5511aff29d1db5fe419ef99a
shayestehhs <shayesthhs1@gmail.com>
2022-12-07 14:51:43 +0330

    Remove tasks from config file

--- regions/tasks.py
+++ regions/tasks.py
@@ -12,11 +12,3 @@ def get_new_images():
     pass
 
 
-def call_download_images_celery_task(instance: Region):
-    end = datetime.now()
-    start = end - timedelta(days=30)
-    geom = get_geojson_by_polygon(instance.polygon)
-    args = (start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'),
-            geom, instance.user_id, instance.id, instance.dates)
-    task = celery_app.send_task("download_images", args)
-    return task
