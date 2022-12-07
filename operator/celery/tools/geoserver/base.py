194699742ad6daaa4822f7e604a061aa828ac0eb
shayestehhs <shayesthhs1@gmail.com>
2022-12-07 10:43:04 +0330

    seprate the server files

--- operator/celery/tools/geoserver/base.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-from geoserver.catalog import Catalog
-
-cat = Catalog(f'http://{environ["GEOSERVER_HOST"]}:{environ["GEOSERVER_PORT"]}/geoserver/rest/',
-              username=environ["GEOSERVER_USER"],
-              password=environ["GEOSERVER_PASS"])
