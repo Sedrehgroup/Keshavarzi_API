5e695ba805a7a4dc53f7c3645c046d9d092730dc
shayestehhs <shayesthhs1@gmail.com>
2022-12-08 15:19:31 +0330

    Comment collectstatic command

--- start_api.sh
+++ start_api.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+celery -A config beat -l DEBUG -f logs/beat.log --scheduler django_celery_beat.schedulers:DatabaseScheduler
+
 python manage.py wait_for_db
 python manage.py wait_for_celery
 
 python manage.py makemigrations --noinput
 python manage.py migrate
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 
 python manage.py geoserver_startup
 
