e24cfd240633f4e32fb4938a630a87ee027df637
shayestehhs <shayesthhs1@gmail.com>
2022-12-15 10:07:37 +0330

    Update the get_weather_forcast functionality

--- regions/tools/cache_tools.py
+++ regions/tools/cache_tools.py
@@ -1,19 +1,49 @@
+import logging
 import requests
 
-from os import environ
+from os import environ, getenv
 from django.core.cache import cache
 from datetime import datetime
 
 API_KEY = environ["TOMORROW_API_KEY"]
+TOMORROW_REQUEST_TIMEOUT = getenv("TOMORROW_REQUEST_TIMEOUT", 2)
+TOMORROW_REQUEST_URL = "https://api.tomorrow.io/v4/timelines"
+logger = logging.getLogger("cache_tools")
 
-def get_weather_forcast(region_id, lat, lon, start_time="now", end_time="nowPlus14d", time_steps="1d"):
-    cached_weather = cache.get(region_id)
+
+def initialize_keys_and_index():
+    value = {"keys": [API_KEY], "index": 0}
+    cache.set(key="keys_and_index")
+    return value
+
+
+def get_api_key():
+    keys_and_index = cache.get("keys_and_index")
+    if not keys_and_index:
+        keys_and_index = initialize_keys_and_index()
+
+    keys, index = keys_and_index["keys"], keys_and_index["index"]
+    keys_and_index.update({"index": index + 1})
+
+    cache.set(key="keys_and_index", value=keys_and_index)
+    return keys[index]
+
+
+def send_request_to_tomorrow(params):
+    params.update({"apikey": get_api_key()})
+    try:
+        return requests.get(TOMORROW_REQUEST_URL, params,
+                            timeout=TOMORROW_REQUEST_TIMEOUT)
+    except requests.exceptions.ReadTimeout as e:
+        return send_request_to_tomorrow(params)
+
+
+def get_weather_forcast(key, lat, lon,
+                        start_time="now", end_time="nowPlus14d", time_steps="1d"):
+    cached_weather = cache.get(key)
     if cached_weather:
         return cached_weather
-    url = "https://api.tomorrow.io/v4/timelines"
-    api_key = API_KEY
     params = {
-        "apikey": api_key,
         "fields": "humidity,temperatureMin,temperatureMax",  # Without any space
         "location": f"{lat},{lon}",
         "timesteps": time_steps,
@@ -21,17 +51,12 @@ def get_weather_forcast(region_id, lat, lon, start_time="now", end_time="nowPlus
         "endTime": end_time,
         "units": "metric"
     }
-    response = requests.get(url, params)
+    response = send_request_to_tomorrow(params)
     if response.status_code != 200:
-        if response.status_code == 403:
-            print("API key is not valid any more. change it.")
-            return response.content
-        print(response.status_code)
-        print(response.content)
-        return
-    forcast = response.content
+        logger.critical(response.content)
+
     now = datetime.now()
     midnight = now.replace(hour=23, minute=59, second=59)
-    time_until_end_of_day = (midnight - now).seconds
-    cache.set(key=region_id, value=forcast, timeout=time_until_end_of_day)
+    second_until_end_of_day = (midnight - now).seconds
+    cache.set(key, response.content, timeout=second_until_end_of_day)
     return response.content
