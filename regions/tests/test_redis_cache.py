0220272262d60bd0df5e8ec04d0f863feb2ca0a9
shayestehhs <shayesthhs1@gmail.com>
2022-12-15 14:56:18 +0330

    Fix minor problem in set key for cache data in redis

--- regions/tests/test_redis_cache.py
+++ regions/tests/test_redis_cache.py
@@ -7,14 +7,14 @@ from regions.tools.cache_tools import get_weather_forcast
 
 class TestRedisCache(TestCase):
     def test_get_weather_forcast_function_is_set_cache_correctly(self):
-        region_id = 1
+        key = "test_key"
         lat = "40.75872069597532"
         lon = "-73.98529171943665"
 
-        self.assertIsNone(cache.get(str(region_id)))
-        forcast = get_weather_forcast(region_id, lat, lon)
+        self.assertIsNone(cache.get(key))
+        forcast = get_weather_forcast(key, lat, lon)
         self.assertIsNotNone(forcast)
-        self.assertIsNotNone(cache.get(region_id))
-        self.assertEqual(cache.get(str(region_id)), forcast)
+        self.assertIsNotNone(cache.get(key))
+        self.assertEqual(cache.get(key), forcast)
 
-        cache.delete(region_id)
+        cache.delete(key)
