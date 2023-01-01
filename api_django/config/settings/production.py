03f9e458d586463660f1474c1aacc2f9a9daf352
shayestehhs <shayesthhs1@gmail.com>
2023-01-01 17:47:22 +0330

    Fix DEBUG handling in production

--- api_django/config/settings/production.py
+++ api_django/config/settings/production.py
@@ -1,5 +1,5 @@
 from config.settings.base import *
 
-DEBUG = bool(os.getenv("DEBUG", 0))
+DEBUG = bool(int(os.getenv("DEBUG", 0)))
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', " ").split()
 SECRET_KEY = os.environ.get("SECRET_KEY")
